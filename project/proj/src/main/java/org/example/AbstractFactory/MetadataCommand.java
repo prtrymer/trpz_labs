765e200a431ba2f8a077bdc7892eaab270a64230
prtrymer <chaika.anton@lll.kpi.ua>
2024-11-15 00:04:59 +0200

    resolved troubles

--- project/proj/src/main/java/org/example/AbstractFactory/MetadataCommand.java
+++
@@ -1,8 +0,0 @@
-package org.example.AbstractFactory;
-
-public class MessageCommand implements Command {
-    @Override
-    public void execute() {
-        // Логіка обробки команди MESSAGE
-    }
-}
