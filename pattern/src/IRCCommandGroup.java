1aacd4fca19f8a4d8dec0c4cdeba11c28758cdab
prtrymer <chaika.anton@lll.kpi.ua>
2024-12-12 22:58:04 +0200

    added lab-8

---
+++ pattern/src/IRCCommandGroup.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IRCCommandGroup implements IRCComponent {
+    private List<IRCComponent> components = new ArrayList<>();
+
+    public void addComponent(IRCComponent component) {
+        components.add(component);
+    }
+
+    @Override
+    public void execute() {
+        for (IRCComponent component : components) {
+            component.execute();
+        }
+    }
+}
---
+++ pattern/src/IRCComponent.java
@@ -0,0 +1,3 @@
+public interface IRCComponent {
+    void execute();
+}
